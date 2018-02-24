cced245b30897592ea4077d84eb307864571fc43
Vlad Oleynik <trikaka12@gmail.com>
2018-02-24 12:39:36 +0200

    added link

--- index.php
+++ index.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>myPortfolio</title>
+    <link rel="icon" type="image/png" href="/img/short.jpg"/>
+    <link rel="stylesheet" href=vendor/bootstrap/css/bootstrap.css>  
+    <link rel="stylesheet" href="vendor/devicons/css/devicons.css">
+    <link rel="stylesheet" href="vendor/font-awesom/css/font-awesome.css">
+    
 </head>
 <body>
-    
+   
 </body>
 </html>
