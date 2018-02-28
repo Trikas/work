07088a70f336ccf29108e76d7fd87dc1549dd1f9
Vlad Oleynik <trikaka12@gmail.com>
2018-02-28 19:29:43 +0200

    css and html stable

--- index.php
+++ index.php
@@ -34,12 +34,15 @@
        </div>  
 </nav>
 <div class="main">
-    <div class="container-fluid">
-        <a href="#info"></a> <h1>Oleynik <span style="color:#CC6633;">Vlad</span></h1><br>
-        <p class="infoP">Mariupol,  str Apatova, (066)-934-41-49, (098)-023-20-54,<span style="color:#CC6633;"> vladoleyn@ukr.net</span></p> <br>
-        <p>Имею опыт разработки веб приложений с применением популярных фреймворков. Получаю степень магистра в одном из вузов Украины. Пишите, буду рад сотруднечиству.
+    <div class="container-fluid ">
+        <div class="content">
+        <a href="#info"></a> <h1>Oleynik <span style="color:#237385;">Vlad</span></h1><br>
+        <p class="infoP">Mariupol</p> 
+        <p class="infoP">tel:(066)-934-41-49, (098)-023-20-54</p>
+        <p class="infoP"><span style="color:#237385;">vladoleyn@ukr.net</span></p><hr>
+        <p>Имею опыт разработки веб приложений с применением популярных фреймворков. Получаю степень магистра в одном из вузов Украины. Готов сотрудничать.
         </p> <br><br>
-        <ul class="list-inline list-social-icons mb-0">
+        <ul class="list-inline list-social-icons mb-0 text-right">
             <li class="list-inline-item">
               <a href="https://www.facebook.com/vlad.trikaka?ref=bookmarks">
                 <span class="fa-stack fa-lg">
@@ -47,8 +50,6 @@
                   <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
                 </span>
               </a>
-            </li>
-            <li class="list-inline-item">
             </li>
             <li class="list-inline-item">
               <a href="https://vk.com/trikaka1">
@@ -67,105 +68,113 @@
               </a>
             </li>
           </ul>
+        </div>  
     </div> 
-    <div class="container-fluid skills">
+    <div class="container-fluid skills"> 
       <a href="#education"></a>
-      <h2>Навыки</h2><br>
-      <p>Языки программирования, фреймоврки, инструментарий</p><br>
-      <ul class="list-inline list-icons">
-          <li class="list-inline-item">
-             <i class="devicon-php-plain"></i>
-            </li>
+      <h2 class="text-center">Навыки</h2><hr><br><br>
+      <div class="content"> 
+        <p>Языки программирования, фреймоврки, инструментарий</p><br>
+        <ul class="list-inline list-icons">
             <li class="list-inline-item">
-            <i class="devicon-apache-plain"></i>
-             </li>
-           <li class="list-inline-item">
-                <i class="fa devicon-html5-plain"></i>
-           </li>
-             <li class="list-inline-item">
-            <i class="devicon-css3-plain"></i>
-             </li>
-             <li class="list-inline-item">
-           <i class="devicon-javascript-plain"></i>
-            </li>
+                <i class="devicon-php-plain"></i>
+                </li>
+                <li class="list-inline-item">
+                <i class="devicon-apache-plain"></i>
+                </li>
             <li class="list-inline-item">
-                <i class="devicon-git-plain"></i>  
+                    <i class="fa devicon-html5-plain"></i>
             </li>
-            <li class="list-inline-item">
-                <i class="devicon-github-plain"></i>
-            </li>
-            <li class="list-inline-item">
-                <i class="devicon-mysql-plain"></i>
-            </li>
-             <li class="list-inline-item">
-                <i class="devicon-photoshop-plain"></i>
-            </li>
-            
-            <li class="list-inline-item">
-                <i class="devicon-intellij-plain"></i>
-            </li>
-          </ul> <br><br>
-          <ul class="fa-ul mb-0">
-            <li>
-             <p> <i class="fa-li fa fa-check"></i>
-                Создание клиент серверных приложений с помощью РНР</p></li>
-            <li>
-              <p><i class="fa-li fa fa-check"></i>
-                Верстка веб страниц при помощи передовых фреймерков</p></li>
-            <li>
-              <p><i class="fa-li fa fa-check"></i>
-                Использование систем контроля версий в разработке</p></li>
-          </ul>
+                <li class="list-inline-item">
+                <i class="devicon-css3-plain"></i>
+                </li>
+                <li class="list-inline-item">
+            <i class="devicon-javascript-plain"></i>
+                </li>
+                <li class="list-inline-item">
+                    <i class="devicon-git-plain"></i>  
+                </li>
+                <li class="list-inline-item">
+                    <i class="devicon-github-plain"></i>
+                </li>
+                <li class="list-inline-item">
+                    <i class="devicon-mysql-plain"></i>
+                </li>
+                <li class="list-inline-item">
+                    <i class="devicon-photoshop-plain"></i>
+                </li>
+                
+                <li class="list-inline-item">
+                    <i class="devicon-intellij-plain"></i>
+                </li>
+            </ul> <br><br>
+            <ul class="fa-ul mb-0">
+                <li>
+                <p> <i class="fa-li fa fa-check"></i>
+                    Создание клиент серверных приложений с помощью РНР</p></li>
+                <li>
+                <p><i class="fa-li fa fa-check"></i>
+                    Верстка веб страниц при помощи передовых фреймерков</p></li>
+                <li>
+                <p><i class="fa-li fa fa-check"></i>
+                    Использование систем контроля версий в разработке</p></li>
+            </ul>
+        </div>   
     </div> 
     <div class="container-fluid education">
-        <h2>Образование</h2><br><br>
+        <h2>Образование</h2><hr><br><br>
+        <div class="content"> 
             <h3>Приазовский Государственный Техническй Университет</h3>
                 <p style="font-size: 20px;">Cпециалист по информационным технологиям</p>
                 <p>Получил квалификационный уровень "Бакалавр"</p>
-            <span class="text-right " style="color:#CC6633;"><p style="font-size: 14px;">c 2015г. по 2017г.</p></span>
-        <h3>Кураховский филиал Приднепровского энергостроительного техникума</h3>
-            <p style="font-size: 20px;"> Младший специалист - инженер з вычислительной техники</p>
-            <p>Получил квалификационный уровень "Младший специалист</p>
-        <span class="text-right " style="color:#CC6633;"><p style="font-size: 14px;">c 2013г. по 2015г.</p></span>
-        <h3>  Донецкая Академия Автомобильного Транспорта</h3>
-            <p style="font-size: 20px;">Среднее полное образование</p>
-        <span class="text-right " style="color:#CC6633;"><p style="font-size: 14px;">до 2013г.</p></span>    
+                <span class="text-right " style="color:#CC6633;"><p style="font-size: 14px;">c 2015г. по 2017г.</p></span><hr>
+            <h3>Кураховский филиал Приднепровского энергостроительного техникума</h3>
+                <p style="font-size: 20px;"> Младший специалист - инженер з вычислительной техники</p>
+                <p>Получил квалификационный уровень "Младший специалист</p>
+                <span class="text-right " style="color:#CC6633;"><p style="font-size: 14px;">c 2013г. по 2015г.</p></span><hr>
+            <h3>Донецкая Академия Автомобильного Транспорта</h3>
+                <p style="font-size: 20px;">Среднее полное образование</p>
+                <span class="text-right " style="color:#CC6633;"><p style="font-size: 14px;">до 2013г.</p></span>    
+        </div>
     </div>
     <div class="container-fluid">
-     <h2>Увлечения</h2>
-        <p>Увлечения связанные исключительно с миром I-T. Игра на гитаре в свободное от кодинга время. Чтение русских классиков и всяческой интересной литературы.</p>
-        <p>Путишествия</p>
-        <p>Просмотр телеканала россия К и насыщение всего своего ества культурой </p>
+        <h2>Увлечения</h2><hr><br><br>
+        <div class="content"> 
+            <p>Увлечения связанные исключительно с миром I-T. Игра на гитаре в свободное от кодинга время. Чтение русских классиков и всяческой интересной литературы.</p>
+            <p>Путишествия</p>
+            <p>Просмотр телеканала россия К и насыщение всего своего ества культурой </p>
+        </div>
     </div>
     <div class="container-fluid">
-        <h2>Качества</h2>
-            <ul class="fa-ul mb-0">
-            <li>
-                <p><i class="fa-li fa fa-check"></i>Аккуратность</p></li>   
-            <li> 
-                <p><i class="fa-li fa fa-check"></i>Надежность</p></li>
-            <li>
-                <p><i class="fa-li fa fa-check"></i>Настойчивость</p></li>
-            <li>
-                <p><i class="fa-li fa fa-check"></i>Оптимизм</p>
-            </li>
-            <p style="font-size: 16px;color:#CC6633;">В сумме это хороший программист который не боиться трудных задач. Которым я являюсь</p><br>
-            <li>
-                <p> <i class="fa-li fa fa-check"></i>Организаторские способности</p>
-            </li>
-            <li>
-              <p><i class="fa-li fa fa-check"></i>Самокритичность</p>
-            </li>
-            <li>
-              <p><i class="fa-li fa fa-check"></i> Стрессоустойчивость</p>
-            </li>
-            <li>
-              <p><i class="fa-li fa fa-check"></i>Чувство юмора</p>
-            </li>
-            <p style="font-size: 16px;color:#CC6633;">В сумме это тоже что то великолепное не придумал.</p>
-          </ul>
-    </div>
-     
+        <h2>Качества</h2><hr><br><br>
+        <div class="content"> 
+                <ul class="fa-ul mb-0">
+                <li>
+                    <p><i class="fa-li fa fa-check"></i>Аккуратность</p></li>   
+                <li> 
+                    <p><i class="fa-li fa fa-check"></i>Надежность</p></li>
+                <li>
+                    <p><i class="fa-li fa fa-check"></i>Настойчивость</p></li>
+                <li>
+                    <p><i class="fa-li fa fa-check"></i>Оптимизм</p>
+                </li>
+                <p style="font-size: 16px;color:#CC6633;">В сумме это хороший программист который не боиться трудных задач. Которым я являюсь</p><br>
+                <li>
+                    <p> <i class="fa-li fa fa-check"></i>Организаторские способности</p>
+                </li>
+                <li>
+                <p><i class="fa-li fa fa-check"></i>Самокритичность</p>
+                </li>
+                <li>
+                <p><i class="fa-li fa fa-check"></i> Стрессоустойчивость</p>
+                </li>
+                <li>
+                <p><i class="fa-li fa fa-check"></i>Чувство юмора</p>
+                </li>
+                <p style="font-size: 16px;color:#CC6633;">В сумме это тоже что то великолепное не придумал.</p>
+            </ul>
+        </div>
+    </div>  
 </div>
 </body>
 </html>
